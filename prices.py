55a1bc8695cffb95a3eee349e0f70cb68805bc74
muxazzz <60567035+muxazzz@users.noreply.github.com>
2020-03-15 19:27:22 +0300

    Create prices.py

---
+++ prices.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+start_date = "2020-03-01"
+end_date = "2020-03-15"
+aapl = yf.download("YNDX DSKY.ME", start=start_date, end=end_date)
+close=aapl["Close"]
+empty={'date': ['Volume'], 'col2':['Volume']}
+df = pd.DataFrame(data=empty)
+volume=aapl["Volume"]
+printer1 = close.append(df)
+printer = printer1.append(volume)
+printer1.plot()
+plt.show
+printer.to_csv (r'/Users/mikhailpuchkov/Desktop/miexport_dataframe.csv', index = True, header=True)
